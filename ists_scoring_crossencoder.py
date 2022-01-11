3557031a438a6586e55d45c2be6a044d930435f8
Gabriela Ossowska <gab.ossowska@gmail.com>
2022-01-11 19:00:07 +0100

    save classification model every epoch

--- ists_scoring_crossencoder.py
+++ ists_scoring_crossencoder.py
@@ -1,23 +1,25 @@
 """
+This examples trains a CrossEncoder for the STSbenchmark task. A CrossEncoder takes a sentence pair
+as input and outputs a label. Here, it output a continious labels 0...1 to indicate the similarity between the input pair.
+
+It does NOT produce a sentence embedding and does NOT work for individual sentences.
 
 Usage:
-python ists_classification_crossencoder.py
-
+python training_stsbenchmark.py
 """
 from torch.utils.data import DataLoader
 import math
 from sentence_transformers import LoggingHandler, util
 from sentence_transformers.cross_encoder import CrossEncoder
-from sentence_transformers.cross_encoder.evaluation import CEBinaryClassificationEvaluator, CESoftmaxAccuracyEvaluator
-from sentence_transformers.readers import InputExample
+from sentence_transformers.cross_encoder.evaluation import CECorrelationEvaluator
+from sentence_transformers import InputExample
 import logging
 from datetime import datetime
+import sys
 import os
+import gzip
 import csv
 from math import floor
-import torch
-import numpy as np
-import argparse
 
 #### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -26,7 +28,6 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
                     handlers=[LoggingHandler()])
 logger = logging.getLogger(__name__)
 #### /print debug information to stdout
-
 
 # parser = argparse.ArgumentParser(description='Read paths and number of epochs')
 
@@ -61,18 +62,28 @@ test_file = 'test_headlines.tsv'
 
 data_name = 'headlines'
 
-num_epochs = 2
+num_epochs = 1
 
 
-label2int = {"EQUI": 0, "REL": 1, "SIMI": 2, "SPE1": 3, "SPE2": 4, "OPPO": 5, "ALIC": 6, "NOALI": 7}
+#Define our Cross-Encoder
+num_epochs = 1
+model_save_path = 'output/training_stsbenchmark-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-logger.info("Read train dataset")
+#We use distilroberta-base as base model and set num_labels=1, which predicts a continous score between 0 and 1
+model = CrossEncoder('roberta-base', num_labels=1)
+
+
+
+# Read STSb dataset
+logger.info("Read STSbenchmark train dataset")
+
 samples = []
 with open(os.path.join(train_path, train_file), 'r', encoding='utf8') as fIn:
     reader = csv.DictReader(fIn, delimiter='\t', quoting=csv.QUOTE_NONE)
     for row in reader:
-        samples.append(InputExample(texts=[row['chunk1'], row['chunk2']], label=label2int[row['type'].split('_')[0]]))
-        #samples.append(InputExample(texts=[row['chunk2'], row['chunk1']], label=label2int[row['type']]))
+        score = float(row['y_type']) / 5.0
+        samples.append(InputExample(texts=[row['x1'], row['x2']], label=score))
+        #samples.append(InputExample(texts=[row['x2'], row['x1']], label=label2int[row['y_score']]))
 
 
 split = 0.8
@@ -84,56 +95,56 @@ test_samples = []
 with open(os.path.join(test_path, test_file), 'r', encoding='utf8') as fIn:
     reader = csv.DictReader(fIn, delimiter='\t', quoting=csv.QUOTE_NONE)
     for row in reader:
-        #test_samples.append(InputExample(texts=[row['chunk1'], row['chunk2']], label=label2int[row['type']]))
-        test_samples.append([row['chunk1'], row['chunk2']])
+        #test_samples.append(InputExample(texts=[row['x1'], row['x2']], label=label2int[row['y_score']]))
+        test_samples.append([row['x1'], row['x2']])
 
 #Configuration
 train_batch_size = 16
-
-output = 'output/'
-model_save_path = train_path + 'output/training_classification-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")+'_epochs_'+str(num_epochs)
-
-#We use distilroberta-base with a single label, i.e., it will output a value between 0 and 1 indicating the similarity of the two questions
-model = CrossEncoder('roberta-base', num_labels=8)
 
 # We wrap train_samples (which is a List[InputExample]) into a pytorch DataLoader
 train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=train_batch_size)
 
 
 # We add an evaluator, which evaluates the performance during training
-evaluator = CESoftmaxAccuracyEvaluator.from_input_examples(dev_samples, name='Classification eval')
+evaluator = CECorrelationEvaluator.from_input_examples(dev_samples, name='Scoring eval')
 
 
 # Configure the training
 warmup_steps = math.ceil(len(train_dataloader) * num_epochs * 0.1) #10% of train data for warm-up
 logger.info("Warmup-steps: {}".format(warmup_steps))
 
-def callback_save(score, epoch, steps):
-    model.save(model_save_path + '_current_' + str(epoch))
-    print("Saved model to {}".format(model_save_path + '_current_' + str(epoch)))
 
 # Train the model
 model.fit(train_dataloader=train_dataloader,
           evaluator=evaluator,
           epochs=num_epochs,
-          evaluation_steps=5000,
           warmup_steps=warmup_steps,
-          output_path=model_save_path,
-          callback=callback_save)
+          output_path=model_save_path)
+
+
+##### Load model and eval on test set
+model = CrossEncoder(model_save_path)
+
+# evaluator = CECorrelationEvaluator.from_input_examples(test_samples, name='sts-test')
+# evaluator(model)
+
+similarity_scores = model.predict(test_samples)
+print(similarity_scores)
 
 predictions = model.predict(test_samples)
 print(predictions)
 
-pred_types = []
+pred_scores = []
+pred_cont_scores = []
+pred_cont_int_scores = []
 for p in predictions:
     print(p)
-    pred_type = np.argmax(p)
-    print(pred_type)
-    print(list(label2int.keys())[list(label2int.values()).index(pred_type)])
-    pred_types.append(list(label2int.keys())[list(label2int.values()).index(pred_type)])
+    pred_scores.append(p)
+    pred_cont_scores.append(p * 5)
+    pred_cont_int_scores.append(round(p * 5))
     
 
-result = 'results/' + data_name + '_epochs_' + str(num_epochs)
+result = 'results/' + data_name + + '_epochs_' + str(num_epochs) + '_scoring.tsv'
 if os.path.exists(os.path.join(test_path, result)):
   os.remove(os.path.join(test_path, result))
 
@@ -141,16 +152,13 @@ with open(os.path.join(test_path, test_file), 'r', encoding='utf8') as fIn, open
     lines_count = 0
     for line in fIn.readlines():
         if (lines_count == 0):
-            res_line = line[:-1] + '\t' + 'pred_types\n'
+            res_line = line[:-1] + '\t' + 'pred_scores\t' + 'pred_cont_scores\t' + 'pred_cont_scores\n'
             lines_count += 1
             print(res_line)
             result.write(res_line)
         else:
-            res_line = line[:-1] + '\t' + str(pred_types[lines_count - 1]) + '\n'
+            res_line = line[:-1] + '\t' + str(pred_scores[lines_count - 1]) + '\t' + str(pred_cont_scores[lines_count - 1]) 
+            + '\t' + str(pred_cont_int_scores[lines_count - 1]) + '\n'
             lines_count += 1
             print(res_line)
             result.write(res_line)
-
-
-
-
